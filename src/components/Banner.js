a24950b6b88e9ac8fa42b80c77a3a72d3cb415f6
Suk0803 <jaesek1223@icloud.com>
2024-03-10 08:53:45 +0900

    firts commit

---
+++ src/components/Banner.js
@@ -0,0 +1,114 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/sss_logo.png";
+import { ArrowRightCircle, UiRadios } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Banner = () => {
+	const [loopNum, setLoopNum] = useState(0);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [text, setText] = useState("");
+	const [delta, setDelta] = useState(300 - Math.random() * 100);
+	const [index, setIndex] = useState(1);
+	const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+	const period = 2000;
+
+	useEffect(() => {
+		let ticker = setInterval(() => {
+			tick();
+		}, delta);
+
+		return () => {
+			clearInterval(ticker);
+		};
+	}, [text]);
+
+	const tick = () => {
+		let i = loopNum % toRotate.length;
+		let fullText = toRotate[i];
+		let updatedText = isDeleting
+			? fullText.substring(0, text.length - 1)
+			: fullText.substring(0, text.length + 1);
+
+		setText(updatedText);
+
+		if (isDeleting) {
+			setDelta((prevDelta) => prevDelta / 2);
+		}
+
+		if (!isDeleting && updatedText === fullText) {
+			setIsDeleting(true);
+			setIndex((prevIndex) => prevIndex - 1);
+			setDelta(period);
+		} else if (isDeleting && updatedText === "") {
+			setIsDeleting(false);
+			setLoopNum(loopNum + 1);
+			setIndex(1);
+			setDelta(500);
+		} else {
+			setIndex((prevIndex) => prevIndex + 1);
+		}
+	};
+
+	return (
+		<section className="banner" id="home">
+			<Container>
+				<Row className="aligh-items-center">
+					<Col xs={12} md={6} xl={7}>
+						<TrackVisibility>
+							{({ isVisible }) => (
+								<div
+									className={
+										isVisible ? "animate__animated animate__fadeIn" : ""
+									}>
+									<span className="tagline">Sungil Software Study</span>
+									<h1>
+										Developing, Studying, Dreaming, Growing, and Advancing as a
+										group of students.
+										{/* {`Hi! We Are SSS`}{" "}
+										<span
+											className="txt-rotate"
+											dataPeriod="1000"
+											data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'>
+											<span className="wrap">{text}</span>
+										</span> */}
+									</h1>
+									<p>
+										성일정보고등학교의 Sungil Software Study에 오신 것을
+										환영합니다! 저희는 개발자를 꿈꾸는 열정적인 학생들의
+										모임으로, 성일정보고등학교 내에서 활발하게 활동하고 있는
+										개발 스터디 그룹입니다. 선후배 간의 멘토멘티 시스템을 통해,
+										우리는 서로의 지식과 경험을 공유하며 함께 성장해가고
+										있습니다.
+									</p>
+									<a href="https://forms.gle/1ky2UgWJuQhPderm9">
+										<button onClick={() => console.log("connect")}>
+											SSS 신청하기 <ArrowRightCircle size={25} />
+										</button>
+									</a>
+								</div>
+							)}
+						</TrackVisibility>
+					</Col>
+					<Col xs={12} md={6} xl={5}>
+						<TrackVisibility>
+							{({ isVisible }) => (
+								<div
+									className={
+										isVisible ? "animate__animated animate__zoomIn" : ""
+									}>
+									<img
+										src={headerImg}
+										alt="Header Img"
+										style={{ borderRadius: 25 }}
+									/>
+								</div>
+							)}
+						</TrackVisibility>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
+};
---
+++ src/components/NavBar.js
@@ -0,0 +1,107 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/img/logo.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/githubicon.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
+
+export const NavBar = () => {
+	const [activeLink, setActiveLink] = useState("home");
+	const [scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const onScroll = () => {
+			if (window.scrollY > 50) {
+				setScrolled(true);
+			} else {
+				setScrolled(false);
+			}
+		};
+
+		window.addEventListener("scroll", onScroll);
+
+		return () => window.removeEventListener("scroll", onScroll);
+	}, []);
+
+	const onUpdateActiveLink = (value) => {
+		setActiveLink(value);
+	};
+
+	return (
+		<Router>
+			<Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+				<Container>
+					<Navbar.Brand href="/">
+						<img src={logo} alt="Logo" style={{ width: 250 }} />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav">
+						<span className="navbar-toggler-icon"></span>
+					</Navbar.Toggle>
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ms-auto">
+							<Nav.Link
+								href="#home"
+								className={
+									activeLink === "home" ? "active navbar-link" : "navbar-link"
+								}
+								onClick={() => onUpdateActiveLink("home")}>
+								Home
+							</Nav.Link>
+							<Nav.Link
+								href="#skills"
+								className={
+									activeLink === "skills" ? "active navbar-link" : "navbar-link"
+								}
+								onClick={() => onUpdateActiveLink("skills")}>
+								Curriculum
+							</Nav.Link>
+							<Nav.Link
+								href="#projects"
+								className={
+									activeLink === "projects"
+										? "active navbar-link"
+										: "navbar-link"
+								}
+								onClick={() => onUpdateActiveLink("projects")}>
+								Projects
+							</Nav.Link>
+						</Nav>
+						<span className="navbar-text">
+							<div className="social-icon">
+								<a
+									href="https://www.instagram.com/sungil_sss/"
+									target="_blank"
+									rel="noopener noreferrer">
+									<img src={navIcon2} alt="ig" />
+								</a>
+							</div>
+							<div className="social-icon">
+								<a
+									href="https://www.instagram.com/sungil_sss/"
+									target="_blank"
+									rel="noopener noreferrer">
+									<img src={navIcon3} alt="ig" />
+								</a>
+							</div>
+							<a
+								href="https://forms.gle/1ky2UgWJuQhPderm9"
+								target="_blank"
+								rel="noopener noreferrer">
+								<button className="vvd">
+									<span>Join SSS</span>
+								</button>
+							</a>
+							{/* <HashLink to="#connect">
+								<button className="vvd">
+									<span>Join SSS</span>
+								</button>
+							</HashLink> */}
+						</span>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</Router>
+	);
+};
